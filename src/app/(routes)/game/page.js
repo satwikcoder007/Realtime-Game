e9897586e56a13cd29c31f8364a512ee5ad8af7d
satwikcoder007 <satwikcoder03@gmail.com>
2024-06-20 23:44:59 +0530

    bug fixed 3

--- src/app/(routes)/game/page.js
+++ src/app/(routes)/game/page.js
@@ -11,7 +11,7 @@ export default function Page() {
   const [loading,setLoading] = useState(1);
   const [gamePass,setGamePass] = useState({
     room:null,
-    move:null,
+    move:'O',
   });
   const [turn,setTurn] = useState(null);
 
